cd7923b7e0e88e4c5375c5c2ef08fea86389f732
Dief Bell <dief.bell@grabcad.com>
2023-08-01 21:58:23 +0100

    Benchmark script works

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,57 +1,60 @@
-// import { type Keyboard, type Mouse } from "@minecraftts/seraph";
-// import { Camera } from "./camera/Camera";
-// import { constructCamera } from "./camera/ICamera";
-// import { Renderer } from "./renderer/Renderer";
-// import * as os from "os";
-// import { Scene } from "./scene/Scene";
-// import { Sphere } from "./scene/Sphere";
+import { type Keyboard, type Mouse } from "@minecraftts/seraph";
+import { Camera } from "./camera/Camera";
+import { constructCamera } from "./camera/ICamera";
+import { Renderer } from "./renderer/Renderer";
+import * as os from "os";
+import { mainScene } from "./prefabs/scenes";
 
-// const WIDTH = 1280;
-// const HEIGHT = 720;
-// const NUM_RENDER_THREADS = Math.floor(os.cpus().length / 2);
+const NUM_RUNS = 10;
 
-// const keyboardStub: Partial<Keyboard> = {
-// 	getKeyDown: () => false
-// };
+const WIDTH = 420;
+const HEIGHT = 420;
+const NUM_RENDER_THREADS = Math.floor(os.cpus().length - 1);
 
-// const mouseStub: Partial<Mouse> = {
-// 	getMouseX: () => WIDTH / 2,
-// 	getMouseY: () => HEIGHT / 2,
-// 	getButtonDown: () => false
-// };
+const keyboardStub: Partial<Keyboard> = {
+	getKeyDown: () => false
+};
 
-// const camera = constructCamera(
-// 	Camera,
-// 	WIDTH,
-// 	HEIGHT,
-// 	keyboardStub as Keyboard,
-// 	mouseStub as Mouse,
-// 	90 * Math.PI / 180,
-// 	0.1,
-// 	100
-// );
+const mouseStub: Partial<Mouse> = {
+	getMouseX: () => WIDTH / 2,
+	getMouseY: () => HEIGHT / 2,
+	getButtonDown: () => false
+};
 
-// const scene = new Scene([
-// 	new Sphere(
-// 		[ 0, 0, 0 ],
-// 		0.5,
-// 		[ 255, 0, 255 ]
-// 	)
-// ]);
+const camera = constructCamera(
+	Camera,
+	WIDTH,
+	HEIGHT,
+	keyboardStub as Keyboard,
+	mouseStub as Mouse,
+	90 * Math.PI / 180,
+	0.1,
+	100
+);
 
-// const renderer = new Renderer(1280, 720, NUM_RENDER_THREADS, camera, scene);
+const scene = mainScene;
+
+const renderer = new Renderer(1280, 720, NUM_RENDER_THREADS, camera, scene);
 
 
-// const main = async() =>
-// {
-// 	let cont = true;
-// 	process.on("SIGINT", () => { cont = false; });
+let totalTime = 0;
+const main = async() =>
+{
+	let cont = true;
+	process.on("SIGINT", () => { cont = false; });
 	
-// 	while(cont)
-// 	{
-// 		const render = await renderer.render();
-// 		console.info(`Render completed in \x1b[1m\x1b[32m${ render.time.toFixed(2) } ms\x1b[0m.`);
-// 	}
+	for(let i = 0; i <= NUM_RUNS; i++)
+	{
+		const render = await renderer.render();
+		console.info(`Render completed in \x1b[1m\x1b[32m${ render.time.toFixed(2) } ms\x1b[0m.`);
 
-// };
-// main();
+        if(!cont) break;
+
+        // don't count very first run
+        if(i > 0) totalTime += render.time;
+	}
+
+    console.log(`\nCompleted ${NUM_RUNS} renders in an average of ${(totalTime/NUM_RUNS).toFixed(2)} ms.`)
+
+};
+main();
